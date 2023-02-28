1989974a8ab915de72cb5e2f7bb2590a4ab539a2
Iago López <iago@entornos.com>
2023-02-28 19:46:15 +0100

    Tercer commit, añadiendo Javadoc

---
+++ TAREA04/doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"TAREA04"}];updateSearchResults();
--- TAREA04/src/cuentas/CCuenta.java
+++ TAREA04/src/cuentas/CCuenta.java
@@ -1,8 +1,16 @@
 package cuentas;
 
+/**
+ * Clase CCuenta que ejecuta las operaciones que imprime Main.java
+ * 
+ * @author Iago
+ * @version 1.0
+ * @since 1.0
+ */
 public class CCuenta {
 
 	private String getNombre() {
+		// Getter del atributo nombre
 		return nombre;
 	}
 
@@ -42,22 +50,42 @@ public class CCuenta {
 	public CCuenta() {
 	}
 
+	/**
+	 * Lista de parámetros:
+	 * 
+	 * @param nom  Nombre del usuario (String)
+	 * @param cue  Nombre de la cuenta (String)
+	 * @param sal  Valor de la cuenta (Double)
+	 * @param tipo Valor del interés (Double)
+	 */
 	public CCuenta(String nom, String cue, double sal, double tipo) {
 		nombre = nom;
 		cuenta = cue;
 		saldo = sal;
 	}
 
+	/**
+	 * Devuelve el saldo de la Cuenta
+	 * 
+	 * @return Saldo
+	 */
 	public double estado() {
 		return saldo;
 	}
 
+	/**
+	 * Método que ingresa el parametro cantidad si es mayor a 0.
+	 */
 	public void ingresar(double cantidad) throws Exception {
 		if (cantidad < 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
 		saldo = saldo + cantidad;
 	}
 
+	/**
+	 * Método que retira el parametro cantidad si es mayor a 0 y el saldo es
+	 * suficiente.
+	 */
 	public void retirar(double cantidad) throws Exception {
 		if (cantidad <= 0)
 			throw new Exception("No se puede retirar una cantidad negativa");
